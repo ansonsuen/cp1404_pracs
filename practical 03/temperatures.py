504eba6da21d741f517142881ba3884e956346c3
anson <ansonsuen32@gmail.com>
2020-04-03 19:20:34 +0800

    add convert_fahrenheit_to_celsius function

--- practical 03/temperatures.py
+++ practical 03/temperatures.py
@@ -3,3 +3,6 @@ def convert_celsius_to_fahrenheit(celsius):
     return celsius * 9.0 / 5 + 32
 
 
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """Convert fahrenheit to celsius."""
+    return 5 / 9 * (fahrenheit - 32)
