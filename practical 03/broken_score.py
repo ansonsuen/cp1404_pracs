d99844232086153237ff19a2088f65f09b753678
anson <ansonsuen32@gmail.com>
2020-04-03 20:59:39 +0800

    add score_grade_determination function

---
+++ practical 03/broken_score.py
@@ -0,0 +1,16 @@
+def score_grade_determination(score):
+    """determine the grades of scores"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+
+
+
+
