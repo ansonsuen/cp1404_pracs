4ac5b49ed5e46d91b6859b26a054576d8b7de52b
anson <ansonsuen32@gmail.com>
2020-04-03 18:52:55 +0800

    add password_check as function

--- practical 03/password_check.py
+++ practical 03/password_check.py
@@ -3,9 +3,10 @@ MINIMUM_LENGTH = 4
 
 def password_check():
     """Get a password of valid size and print asterisks."""
-    password = int(input("Enter at least {} characters for password".format(MINIMUM_LENGTH)))
-    while password < MINIMUM_LENGTH:
-        password = int(input("Enter at least {} characters for password".format(MINIMUM_LENGTH)))
+    password = input("Enter at least {} characters for password".format(MINIMUM_LENGTH))
+    while len(password) < MINIMUM_LENGTH:
+        password = input("Enter at least {} characters for password".format(MINIMUM_LENGTH))
     print("*" * len(password))
 
 
+password_check()
