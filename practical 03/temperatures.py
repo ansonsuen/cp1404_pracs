8a8f7a5803e601abaebd6d4deb9af01a7b86128e
anson <ansonsuen32@gmail.com>
2020-04-03 19:34:33 +0800

    add main function

--- practical 03/temperatures.py
+++ practical 03/temperatures.py
@@ -6,3 +6,32 @@ def convert_celsius_to_fahrenheit(celsius):
 def convert_fahrenheit_to_celsius(fahrenheit):
     """Convert fahrenheit to celsius."""
     return 5 / 9 * (fahrenheit - 32)
+
+
+MENU = """
+C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+
+
+def main():
+    print(MENU)
+    choice = str(input(">>>")).upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("celsius: "))
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            print("Result:{:.2f}".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("fahrenheit: "))
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            print("Result:{:.2f}".format(celsius))
+        else:
+            print("invalid")
+        print(MENU)
+        choice = str(input(">>>"))
+    print("thank You")
+
+
+if __name__ == '__main__':
+    main()
