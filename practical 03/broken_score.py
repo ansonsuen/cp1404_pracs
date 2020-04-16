ec82d7772e54842a836810a15836d8cbe367996e
anson <ansonsuen32@gmail.com>
2020-04-16 16:25:02 +0800

    add print_report function

--- practical 03/broken_score.py
+++ practical 03/broken_score.py
@@ -1,3 +1,6 @@
+import random
+
+
 def score_status_determination(score):
     """determine the grades of scores"""
     if score < 0 or score > 100:
@@ -12,7 +15,9 @@ def score_status_determination(score):
 
 def main():
     """print status due to the score"""
+
     score = float(input("Enter score: "))
+
     print(score_status_determination(score))
 
 
---
+++ practical 04/lists_warmup.py
@@ -0,0 +1,40 @@
+def lst_of_numbers():
+    numbers = [3, 1, 4, 1, 5, 9, 2]
+    # numbers[0] =[3]
+    # numbers[-1]=[2]
+    # numbers[3]= [1]
+    # numbers[:-1]=[3, 1, 4, 1, 5, 9]
+    # numbers[3:4]= [1]
+    # 5 in numbers = True
+    # 7 in numbers= False
+    # "3" in numbers= False
+    print(numbers + [6, 5, 3])
+
+
+lst_of_numbers()
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(
+            input("Enter income for month {}: ".format(month)))
+        incomes.append(income)
+
+    print_report(incomes)
+
+
+def print_report(incomes):
+    """Print report based on incomes."""
+    # Note that we do not need to pass in number_of_months
+    # because we know the length of the incomes list
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month, income in enumerate(incomes):
+        total += income
+        print("Month {:2} - Income: ${:10.2f} \
+        Total: ${:10.2f}".format(month + 1, income, total))
+
+
+main()
