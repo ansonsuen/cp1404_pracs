1524c5fd6accce6c5d3048b06de39fcd1bbb99d9
anson <ansonsuen32@gmail.com>
2020-04-22 18:09:39 +0800

    add main

--- practical 02/capitalist_conrad.py
+++ practical 02/capitalist_conrad.py
@@ -1,34 +1,30 @@
-
 import random
 
 MAX_INCREASE = 0.1
 MAX_DECREASE = 0.05
 MIN_PRICE = 0.01
+import random
+
+MAX_INCREASE = 0.1  # 10%
+MAX_DECREASE = 0.05  # 5%
+MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
-OUTPUT_FILE = "stock_output.txt"
-out_file = open(OUTPUT_FILE, 'w')
-price = INITIAL_PRICE
-day = 0
-print("Starting price: ${:,.2f}".format(price), file=out_file.close())
-while MIN_PRICE <= price <= MAX_PRICE:
-    day += 1
-    price_change = 0
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
-    if random.randint(1, 2) == 1:
-        # generate a random floating-point number
-        # between 0 and MAX_INCREASE
-        price_change = random.uniform(5,25)
-    else:
-        # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
-        price_change = random.uniform(10,20)
 
-    price *= (1 + price_change)
-    print("On day {}, price is ${:,.2f}".format(day, price), file=out_file.close())
-    out_file.close()
+def main():
+    """create a stock price simulator """
+    price = INITIAL_PRICE
+    print("${:,.2f}".format(price))
+    while MIN_PRICE <= price <= MAX_PRICE:
+        price_change = 0
+        if random.randint(1, 2) == 1:
+            price_change = random.uniform(0, MAX_INCREASE)
+        else:
+            price_change = random.uniform(-MAX_DECREASE, 0)
+        price *= (1 + price_change)
+        print("${:,.2f}".format(price))
+
+
 if __name__ == '__main__':
-   main()
-
+    main()
