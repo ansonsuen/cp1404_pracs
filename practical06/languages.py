9af32728e86e304def8bd8e7a03504aa564be91e
anson <ansonsuen32@gmail.com>
2020-05-02 15:37:37 +0800

    add main function(wrongly push it as once from the previous commit)

--- practical06/languages.py
+++ practical06/languages.py
@@ -2,7 +2,7 @@ from practical06.programming_languages import ProgrammingLanguage
 
 
 def main():
-    """programming language program, using programming language class."""
+    """programming a language program, using programming language class."""
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
