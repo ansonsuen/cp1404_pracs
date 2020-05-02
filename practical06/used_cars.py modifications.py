d2bdd2781d79817af3c80fa1bff45fc4fa7cfc3f
anson <ansonsuen32@gmail.com>
2020-05-02 15:39:33 +0800

    add main function(wrongly push it as once from the previous commit)

--- practical06/used_cars.py modifications.py
+++ practical06/used_cars.py modifications.py
@@ -2,7 +2,7 @@ from practical06.car import Car
 
 
 def main():
-    """Demo test code to show how to use car class."""
+    """Demo test code to show how the usage of car class."""
     my_car = Car("My car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
