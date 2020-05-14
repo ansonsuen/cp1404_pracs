47e18560c6d5aa240178e654a3b05105fb25a881
anson <ansonsuen32@gmail.com>
2020-05-14 18:08:41 +0800

    add a _init_ in unreliable_car

--- practical08/unreliable_car.py
+++ practical08/unreliable_car.py
@@ -4,6 +4,11 @@ from random import randint
 class unreliable_car:
     """An unreliable version of a car."""
 
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
 
 
 
