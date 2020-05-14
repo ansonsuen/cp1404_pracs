8a8825dbe9bf78690a18f92d8529933306727551
anson <ansonsuen32@gmail.com>
2020-05-14 18:09:34 +0800

    add fuction drive  in the class of unreliable_car

--- practical08/unreliable_car.py
+++ practical08/unreliable_car.py
@@ -9,6 +9,14 @@ class unreliable_car:
         super().__init__(name, fuel)
         self.reliability = reliability
 
+    def drive(self, distance):
+        """Drive the car based on reliability."""
+        random_number = randint(1, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        driven_distance = super().__init__(distance)
+        return driven_distance
+
 
 
 
