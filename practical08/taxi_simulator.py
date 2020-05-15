dae2924e22f1d55a16b01a290160e7450ec6b530
anson <ansonsuen32@gmail.com>
2020-05-15 14:00:43 +0800

    add run_test function

--- practical08/taxi_simulator.py
+++ practical08/taxi_simulator.py
@@ -42,3 +42,37 @@ def main():
     display_taxi(taxis)
 
 
+def run_tests():
+    """Run tests to show workings of Car and Taxi classes."""
+    bus = Car("Datsun", 180)
+    bus.drive(30)
+    print("fuel =", bus.fuel)
+    print("odo =", bus.odometer)
+    bus.drive(55)
+    print("fuel =", bus.fuel)
+    print("odo = ", bus.odometer)
+    print(bus)
+
+    # drive bus (input/loop is oblivious to fuel)
+    distance = int(input("Drive how far? "))
+    while distance > 0:
+        travelled = bus.drive(distance)
+        print("{} travelled {}".format(str(bus), travelled))
+        distance = int(input("Drive how far? "))
+
+    t = Taxi("Prius 1", 100)
+    print(t)
+    t.drive(25)
+    print(t, t.get_fare())
+    t.start_fare()
+    t.drive(40)
+    print(t, t.get_fare())
+
+    sst = SilverServiceTaxi("Limo", 100, 2)
+    print(sst, sst.get_fare())
+    sst.drive(10)
+    print(sst, sst.get_fare())
+
+
+if __name__ == '__main__':
+    main()
