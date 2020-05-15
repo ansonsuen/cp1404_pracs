2898db8265398bcb077344db108dad3c330fc733
anson <ansonsuen32@gmail.com>
2020-05-15 14:00:22 +0800

    add main function

--- practical08/taxi_simulator.py
+++ practical08/taxi_simulator.py
@@ -13,3 +13,32 @@ def display_taxi(taxis):
         print("{}-{}".format(i, taxi))
 
 
+def main():
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    menu()
+    choice_menu = str(input(""))
+    while choice_menu != "q".lower():
+        if choice_menu == "c":
+            print("Taxis available:")
+            display_taxi(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            current_taxi = taxis[taxi_choice]
+        elif choice_menu == "d":
+            current_taxi.start_fare()
+            drive_distance = float(input("Drive how far? "))
+            current_taxi.drive(drive_distance)
+            trip_cost = current_taxi.get_fare()
+            print("Your {} trip cost you ${:.2f}".format(current_taxi.name,
+                                                         trip_cost))
+        else:
+            print("Invalid option")
+        print("Bill to date: ${:.2f}".format(total_bill))
+        print(menu())
+        choice_menu = input(">>> ").lower()
+    print("Total trip cost: ${:.2f}".format(total_bill))
+    print("Taxis are now:")
+    display_taxi(taxis)
+
+
