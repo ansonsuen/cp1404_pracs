657807825fcdd1a43546e4c5c598d521119e2891
anson <ansonsuen32@gmail.com>
2020-05-21 17:33:30 +0800

    add main function

---
+++ practical09/cleanup_files.py
@@ -0,0 +1,21 @@
+import os
+
+
+def main():
+    """Cleanup inconsistent song lyrics file names."""
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
+
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, new_name)
+            os.rename(full_name, new_name)
+
+
+
+
+
+main()
+
